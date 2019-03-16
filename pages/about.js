42b14855a8ef2815da01631a23d5a75b8b76f480
Riley-Brown <riley@riley.gg>
2019-03-16 00:03:32 -0400

    Created Navbar component for linking to routes and imported in index and about pages

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,8 @@
+import Link from 'next/link';
+import Navbar from '../components/Navbar';
 const About = () => (
   <div>
+    <Navbar />
     <h1>About my cool Next.js app</h1>
     <p>App to view bitcoin prices using Next.js</p>
   </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+import Link from 'next/link';
+import Navbar from '../components/Navbar';
 const Index = () => (
   <div>
+    <Navbar />
     <h1>Hello from Next.js</h1>
   </div>
 );
