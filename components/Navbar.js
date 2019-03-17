b4276fad9afc2fd9994b0d1e0a2f72d3c0f6769c
Riley-Brown <riley@riley.gg>
2019-03-16 23:30:23 -0400

    added styles to navbar using jsx

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,35 @@
 import Link from 'next/link';
 const Navbar = () => (
-  <ul>
-    <li>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+    </ul>
+    <style jsx>{`
+      ul {
+        background: #444;
+        color: #fff;
+        list-style: none;
+        display: flex;
+      }
+      ul li {
+        font-size: 1.3rem;
+        margin-right: 20px;
+      }
+      ul li a {
+        color: #fff;
+        text-decoration: none;
+      }
+    `}</style>
+  </div>
 );
 
 export default Navbar;
