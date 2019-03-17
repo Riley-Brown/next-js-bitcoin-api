9bd0459d149f371f28942a8d1099721212df6578
Riley-Brown <riley@riley.gg>
2019-03-17 02:41:23 -0400

    Added fetch request to coindesk api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 import Layout from '../components/Layout';
-const Index = () => (
+import fetch from 'isomorphic-unfetch';
+import Prices from '../components/Prices';
+
+const Index = props => (
   <Layout>
     <div>
       <h1>Hello from Next.js</h1>
+      <Prices bpi={props.bpi} />
     </div>
   </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  const data = await res.json();
+
+  return {
+    bpi: data
+  };
+};
 
 export default Index;
