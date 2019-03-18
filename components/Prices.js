8d80a71bb0a6908259a17bcfa3d01391fcadf384
Riley-Brown <riley@riley.gg>
2019-03-17 21:08:21 -0400

    Updated bitcoin price to update every 15 seconds and update state

--- components/Prices.js
+++ components/Prices.js
@@ -1,7 +1,45 @@
+import fetch from 'isomorphic-unfetch';
+
 class Prices extends React.Component {
-  state = {
-    currency: 'USD'
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currency: 'USD',
+      price: this.props.bpi.bpi.USD.rate,
+      description: this.props.bpi.bpi.description,
+      currencyCode: this.props.bpi.bpi.USD.code
+    };
+  }
+
+  componentDidMount() {
+    setInterval(this.getBitcoinData, 15000);
+    this.getBitcoinData();
+  }
+  componentWillUnmount() {
+    clearInterval(this.getBitcoinData);
+  }
+
+  getBitcoinData = () => {
+    fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(api => {
+        this.setState({
+          price: api.bpi.USD.rate,
+          description: api.bpi.USD.description,
+          currencyCode: api.bpi.USD.code
+        });
+        console.log(api.bpi);
+      });
+
+    console.log('running');
+    // this.setState({
+    //   price: '1234'
+    // });
   };
+
   render() {
     let api;
     if (this.props.bpi) {
@@ -9,11 +47,11 @@ class Prices extends React.Component {
         <div>
           <ul className="list-group">
             <li className="list-group-item">
-              Bitcoin rate for {this.props.bpi.bpi.USD.description}:
+              Bitcoin rate for {this.state.description}:
               <span className="badge badge-primary mx-2">
-                {this.props.bpi.bpi.USD.code}
+                {this.state.currencyCode}
               </span>
-              <strong>{this.props.bpi.bpi.USD.rate}</strong>
+              <strong>{this.state.price}</strong>
             </li>
           </ul>
         </div>
